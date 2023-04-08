30975b5a537db2163ee0a9fb14e9dcbc3a81030e
Bholanath Barik <71944044+bholanathbarik9748@users.noreply.github.com>
2023-04-08 13:40:06 +0530

    improve Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <header className="text-cyan-500 body-font bg-current">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" to='/'>
-                        <span className="ml-3 text-xl">🌤 Whether App</span>
+                        <span className="ml-3 text-xl">🌤 Whether jupiter</span>
                     </Link>
                 </div>
             </header>
